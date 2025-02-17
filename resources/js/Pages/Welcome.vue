fc238d9d01dc8588d7e963899519f6a272d35051
JustinLawrenceMS <justin.lawrence.codes@gmail.com>
2025-02-17 15:57:58 -0600

    chore - delete things

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,29 +2,6 @@
 import { reactive, provide } from "vue";
 import Form from "@/Components/Form.vue";
 import PostcardPreview from "@/Components/PostcardPreview.vue";
-defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
-});
-
-function handleImageError() {
-    document.getElementById("screenshot-container")?.classList.add("!hidden");
-    document.getElementById("docs-card")?.classList.add("!row-span-1");
-    document.getElementById("docs-card-content")?.classList.add("!flex-row");
-    document.getElementById("background")?.classList.add("!hidden");
-}
 
 const postcardState = reactive({
     message: '',
